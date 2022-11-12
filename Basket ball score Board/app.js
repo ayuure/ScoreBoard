5c8deff07d719f546c190c027cb3e18091ed6901
ayuure <61334000+ayuure@users.noreply.github.com>
2022-11-12 18:51:46 +0000

    Add files via upload

---
+++ Basket ball score Board/app.js
@@ -0,0 +1,40 @@
+ let homeEl =document.getElementById('p3')
+// console.log(homeEl)
+ let guestEl= document.getElementById('p2')
+// console.log(guestEl)
+// let home1El =document.getElementById('btn_one')
+// console.log(home1El)
+// let home2El = document.getElementById('btn_two')
+// console.log(home2El)
+// let home3El = document.getElementById('btn_three')
+// console.log(home3El)
+// let guest1El = document.getElementById('one_btn')
+// console.log(guest1El)
+// let guest2El =document.getElementById('two_btn')
+// console.log(guest2El)
+// let guest3El=document.getElementById('three_btn')
+// console.log(guest3El)
+
+
+
+function add(i){
+    let currentValue = Number(p3.textContent)
+
+    p3.textContent =  currentValue + i;
+}
+function add2(i){
+    let currentValue = Number(p2.textContent)
+
+    p2.textContent =  currentValue + i;
+
+}
+
+
+ 
+   
+
+
+
+
+
+
